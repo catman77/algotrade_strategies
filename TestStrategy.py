643fb9009de182c75d7ad66447b2e93381bca19e
LeoLucard <leolucard88@gmail.com>
2024-01-19 10:36:38 +0630

    19-01-2024 KNN plus Trend Strategy Complete

--- TestStrategy.py
+++ TestStrategy.py
@@ -29,64 +29,39 @@ class TestStrategy(IStrategy):
 
     # Minimal ROI designed for the strategy.
     # This attribute will be overridden if the config file contains "minimal_roi".
-    # minimal_roi = {
-    #     "60": 0.01,
-    #     "30": 0.02,
-    #     "0": 0.04
-    # }
-
+    minimal_roi = {
+        "15": 0.0,
+        "0": 0.05
+    }
+ 
     # Optimal stoploss designed for the strategy.
     # This attribute will be overridden if the config file contains "stoploss".
-    stoploss = -0.6
+    stoploss = -1
 
     # Trailing stoploss
     trailing_stop = False
-    # trailing_only_offset_is_reached = False
-    # trailing_stop_positive = 0.01
-    # trailing_stop_positive_offset = 0.0  # Disabled / not configured
+    trailing_stop_positive = 0.01
+    trailing_stop_positive_offset = 0.02
+    trailing_only_offset_is_reached = True
 
     # Optimal timeframe for the strategy.
-    timeframe = '5m'
+    timeframe = '1m'
 
     # Run "populate_indicators()" only for new candle.
-    process_only_new_candles = True
-
+    process_only_new_candles = False
     # These values can be overridden in the config.
     use_exit_signal = True
     exit_profit_only = False
-    ignore_roi_if_entry_signal = False
 
     # Number of candles the strategy requires before producing valid signals
-    startup_candle_count: int = 200
+    startup_candle_count: int = 100
 
     # Optional order type mapping.
     order_types = {
-        'entry': 'limit',
-        'exit': 'limit',
+        'entry': 'market',
+        'exit': 'market',
         'stoploss': 'market',
         'stoploss_on_exchange': False
-    }
-
-    # Optional order time in force.
-    order_time_in_force = {
-        'entry': 'GTC',
-        'exit': 'GTC'
-    }
-
-    plot_config = {
-        'main_plot': {
-            'tema': {},
-            'sar': {'color': 'white'},
-        },
-        'subplots': {
-            "MACD": {
-                'macd': {'color': 'blue'},
-                'macdsignal': {'color': 'orange'},
-            },
-            "RSI": {
-                'rsi': {'color': 'red'},
-            }
-        }
     }
 
     # Strategy Constant
@@ -97,6 +72,7 @@ class TestStrategy(IStrategy):
         # For Backtesting
         dataframe = self.adaptiveTrendFinder_2(dataframe)
         dataframe['trend'] = dataframe['trend_direction'].apply(lambda x: x[0])
+        dataframe['trend_period'] = dataframe['trend_direction'].apply(lambda x: x[1])
         dataframe['trend_strength'] = dataframe['trend_direction'].apply(lambda x: x[2])
 
 
@@ -104,13 +80,17 @@ class TestStrategy(IStrategy):
         # trend_direction,detectedPeriod,highestPearsonR = self.adaptiveTrendFinder(dataframe)
         # dataframe['trend'] = trend_direction
         # dataframe['trend_strength'] = highestPearsonR
+        # dataframe['trend_period'] = detectedPeriod
         # KNN Strategy
         dataframe = self.trainModelML(dataframe)
 
-        # print("=======================")
-        print(dataframe.loc[:,['trend','trend_strength']])
+        # MACD Strategy
+        dataframe = self.calculateMACD(dataframe)
+
+        print("=======================")
+        print((dataframe.loc[len(dataframe)-29:,['trend','trend_period','trend_strength','predicted_value','close']]))
         print(datetime.now(timezone.utc))
-        print("=====================")
+        print("=======================")
         return dataframe
 
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
@@ -118,19 +98,19 @@ class TestStrategy(IStrategy):
             (
                 (dataframe['trend'] > 0)
                 &
-                (dataframe['trend_strength'] >= 0.90) 
-                &
                 (dataframe['predicted_value'] > 0)
+                &
+                (dataframe['position'] > 0)
             ),
             'enter_long'] = 1
         
         dataframe.loc[
             (
-                (dataframe['trend'] < 0) 
-                &
-                (dataframe['trend_strength'] >= 0.90)
+                (dataframe['trend'] < 0)
                 &
                 (dataframe['predicted_value'] < 0)
+                &
+                (dataframe['position'] < 0)
             ),
             'enter_short'] = 1
         return dataframe
@@ -138,179 +118,42 @@ class TestStrategy(IStrategy):
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
         dataframe.loc[
             (
-                (dataframe['trend'] < 0) 
-                &
-                (dataframe['trend_strength'] >= 0.90)
+                (dataframe['predicted_value'] < 0)
             ),
             'exit_long'] = 1
         
         dataframe.loc[
             (
-                (dataframe['trend'] > 0)
-                &
-                (dataframe['trend_strength'] >= 0.90)
+                (dataframe['predicted_value'] > 0)
             ),
             'exit_short'] = 1
         return dataframe
     
 
+    def leverage(self, pair: str, current_time: datetime, current_rate: float,
+                 proposed_leverage: float, max_leverage: float, entry_tag: Optional[str],
+                 side: str, **kwargs) -> float:
+
+        return 10
+    
 
     # Helper Function
-    def adaptiveTrendFinder(self,dataframe:DataFrame):
-        # Variable Can Modify
-        devMultiplier = 2.0
+    def calculateMACD(self,dataframe):
+        # Calculate MACD
+        short_window = 12
+        long_window = 26
+        signal_window = 9
 
-        # Calculate Deviation,PersionR,Slope,Intercept
-        stdDev01, pearsonR01, slope01, intercept01 = self.calcDev(self.periods[1],dataframe)
-        stdDev02, pearsonR02, slope02, intercept02 = self.calcDev(self.periods[2],dataframe)
-        stdDev03, pearsonR03, slope03, intercept03 = self.calcDev(self.periods[3],dataframe)
-        stdDev04, pearsonR04, slope04, intercept04 = self.calcDev(self.periods[4],dataframe)
-        stdDev05, pearsonR05, slope05, intercept05 = self.calcDev(self.periods[5],dataframe)
-        stdDev06, pearsonR06, slope06, intercept06 = self.calcDev(self.periods[6],dataframe)
-        stdDev07, pearsonR07, slope07, intercept07 = self.calcDev(self.periods[7],dataframe)
-        stdDev08, pearsonR08, slope08, intercept08 = self.calcDev(self.periods[8],dataframe)
-        stdDev09, pearsonR09, slope09, intercept09 = self.calcDev(self.periods[9],dataframe)
-        stdDev10, pearsonR10, slope10, intercept10 = self.calcDev(self.periods[10],dataframe)
-        stdDev11, pearsonR11, slope11, intercept11 = self.calcDev(self.periods[11],dataframe)
-        stdDev12, pearsonR12, slope12, intercept12 = self.calcDev(self.periods[12],dataframe)
-        stdDev13, pearsonR13, slope13, intercept13 = self.calcDev(self.periods[13],dataframe)
-        stdDev14, pearsonR14, slope14, intercept14 = self.calcDev(self.periods[14],dataframe)
-        stdDev15, pearsonR15, slope15, intercept15 = self.calcDev(self.periods[15],dataframe)
-        stdDev16, pearsonR16, slope16, intercept16 = self.calcDev(self.periods[16],dataframe)
-        stdDev17, pearsonR17, slope17, intercept17 = self.calcDev(self.periods[17],dataframe)
-        stdDev18, pearsonR18, slope18, intercept18 = self.calcDev(self.periods[18],dataframe)
-        stdDev19, pearsonR19, slope19, intercept19 = self.calcDev(self.periods[19],dataframe)
+        dataframe['short_ema'] = dataframe['close'].ewm(span=short_window, adjust=False).mean()
+        dataframe['long_ema'] = dataframe['close'].ewm(span=long_window, adjust=False).mean()
+        dataframe['macd'] = dataframe['short_ema'] - dataframe['long_ema']
+        dataframe['signal_line'] = dataframe['macd'].ewm(span=signal_window, adjust=False).mean()
+        # Generate buy and sell signals
+        dataframe['position'] = 0
+        dataframe.loc[(dataframe['macd'] > dataframe['signal_line']), 'position'] = 1  # Buy signal
+        dataframe.loc[(dataframe['macd'] < dataframe['signal_line']), 'position'] = -1  # Sell signal
 
-        # Find the highest Pearson's R
-        # float highestPearsonR = pearsonR01
-        highestPearsonR = max(pearsonR01, pearsonR02, pearsonR03, pearsonR04, pearsonR05, pearsonR06, pearsonR07, pearsonR08, pearsonR09, pearsonR10, pearsonR11, pearsonR12, pearsonR13, pearsonR14, pearsonR15, pearsonR16, pearsonR17, pearsonR18, pearsonR19)
-
-        # Determine selected length, slope, intercept, and deviations
-        detectedPeriod  = 0
-        detectedSlope   = 0
-        detectedIntrcpt = 0
-        detectedStdDev  = 0
-
-        if highestPearsonR == pearsonR01:
-            detectedPeriod = self.periods[1]
-            detectedSlope = slope01
-            detectedIntrcpt = intercept01
-            detectedStdDev = stdDev01
-        elif highestPearsonR == pearsonR02:
-            detectedPeriod = self.periods[2] 
-            detectedSlope = slope02
-            detectedIntrcpt = intercept02
-            detectedStdDev = stdDev02
-        elif highestPearsonR == pearsonR03:
-            detectedPeriod = self.periods[3]  
-            detectedSlope = slope03
-            detectedIntrcpt = intercept03
-            detectedStdDev = stdDev03
-        elif highestPearsonR == pearsonR04:
-            detectedPeriod = self.periods[4]  
-            detectedSlope = slope04
-            detectedIntrcpt = intercept04
-            detectedStdDev = stdDev04
-        elif highestPearsonR == pearsonR05:
-            detectedPeriod = self.periods[5]  
-            detectedSlope = slope05
-            detectedIntrcpt = intercept05
-            detectedStdDev = stdDev05
-        elif highestPearsonR == pearsonR06:
-            detectedPeriod = self.periods[6]       
-            detectedSlope = slope06
-            detectedIntrcpt = intercept06
-            detectedStdDev = stdDev06
-        elif highestPearsonR == pearsonR07:
-            detectedPeriod = self.periods[7]      
-            detectedSlope = slope07
-            detectedIntrcpt = intercept07
-            detectedStdDev = stdDev07
-        elif highestPearsonR == pearsonR08:
-            detectedPeriod = self.periods[8]       
-            detectedSlope = slope08
-            detectedIntrcpt = intercept08
-            detectedStdDev = stdDev08
-        elif highestPearsonR == pearsonR09:
-            detectedPeriod = self.periods[9]       
-            detectedSlope = slope09
-            detectedIntrcpt = intercept09
-            detectedStdDev = stdDev09
-        elif highestPearsonR == pearsonR10:
-            detectedPeriod = self.periods[10]
-            detectedSlope = slope10
-            detectedIntrcpt = intercept10
-            detectedStdDev = stdDev10
-        elif highestPearsonR == pearsonR11:
-            detectedPeriod = self.periods[11]
-            detectedSlope = slope11
-            detectedIntrcpt = intercept11
-            detectedStdDev = stdDev11
-        elif highestPearsonR == pearsonR12:
-            detectedPeriod = self.periods[12]
-            detectedSlope = slope12
-            detectedIntrcpt = intercept12
-            detectedStdDev = stdDev12
-        elif highestPearsonR == pearsonR13:
-            detectedPeriod = self.periods[13]
-            detectedSlope = slope13
-            detectedIntrcpt = intercept13
-            detectedStdDev = stdDev13
-        elif highestPearsonR == pearsonR14:
-            detectedPeriod = self.periods[14]
-            detectedSlope = slope14
-            detectedIntrcpt = intercept14
-            detectedStdDev = stdDev14
-        elif highestPearsonR == pearsonR15:
-            detectedPeriod = self.periods[15]
-            detectedSlope = slope15
-            detectedIntrcpt = intercept15
-            detectedStdDev = stdDev15
-        elif highestPearsonR == pearsonR16:
-            detectedPeriod = self.periods[16]
-            detectedSlope = slope16
-            detectedIntrcpt = intercept16
-            detectedStdDev = stdDev16
-        elif highestPearsonR == pearsonR17:
-            detectedPeriod = self.periods[17]
-            detectedSlope = slope17
-            detectedIntrcpt = intercept17
-            detectedStdDev = stdDev17
-        elif highestPearsonR == pearsonR18:
-            detectedPeriod = self.periods[18]
-            detectedSlope = slope18
-            detectedIntrcpt = intercept18
-            detectedStdDev = stdDev18
-        elif highestPearsonR == pearsonR19:
-            detectedPeriod = self.periods[19]
-            detectedSlope = slope19
-            detectedIntrcpt = intercept19
-            detectedStdDev = stdDev19
-        else:
-            # Default case
-            raise Exception("Cannot Find Highest PearsonR") 
-        
-        # Calculate start and end price based on detected slope and intercept
-        startPrice = math.exp(detectedIntrcpt + detectedSlope * (detectedPeriod - 1))
-        endPrice = math.exp(detectedIntrcpt)
-        startAtBar = len(dataframe) - detectedPeriod + 1
-
-        # Calculate Upper Upper Price and Upper End price
-        upperStartPrice = startPrice * math.exp(devMultiplier * detectedStdDev)
-        upperEndPrice   =   endPrice * math.exp(devMultiplier * detectedStdDev)
-
-        # Calculate Lower Price and Lower End Price
-        lowerStartPrice = startPrice / math.exp(devMultiplier * detectedStdDev)
-        lowerEndPrice =   endPrice / math.exp(devMultiplier * detectedStdDev)
-
-        # Calculate If Uptrend or Downtrend and how strength is this trend
-        # Also Know how many this trend exist with period
-        # ====== Strategies ======
-        # If EndPrice > StartPrice Uptrend
-        # If EndPrice < StartPrice Downtrend
-        trend_direction = endPrice - startPrice
-
-        return trend_direction,detectedPeriod,highestPearsonR
+        return dataframe
     
 
     def calcDev(self,length:int,dataframe:DataFrame):
@@ -360,13 +203,13 @@ class TestStrategy(IStrategy):
     
     def minimax(self,volume,x,p,min,max):
         volume_array = volume.to_numpy()
-        volume_array = volume_array[:x.name]
+        volume_array = volume_array[:x.name+1]
 
         if(len(volume_array) < p):
             return 0
     
-        hi = np.nan_to_num(np.max(volume_array[-p:]))
-        lo = np.nan_to_num(np.min(volume_array[-p:]))
+        hi = np.nan_to_num(np.max(volume_array[-p+1:]))
+        lo = np.nan_to_num(np.min(volume_array[-p+1:]))
 
         return (max - min) * (volume_array[len(volume_array)-1] - lo)/(hi - lo) + min
     
@@ -381,11 +224,11 @@ class TestStrategy(IStrategy):
         previous_value = close_array[len(close_array) - 2]
         
         if(previous_value > current_value):
-            return 1
-        elif(previous_value < current_value):
             return -1
-        else:
+        elif(previous_value < current_value):
             return 1
+        else:
+            return 0
     
     def get_long_or_short_prediction(self,index,predicte_list):
         predicte_array = predicte_list.to_numpy()
@@ -398,19 +241,49 @@ class TestStrategy(IStrategy):
         previous_value = predicte_array[len(predicte_array) - 2]
         return current_value - previous_value
     
+    def calculate_CCI(self, dataframe,index,diff,ma,p):
+
+        close_array = dataframe['close'].to_numpy()
+        close_array = close_array[:index.name]
+        diff = diff.to_numpy()
+        diff = diff[index.name]
+        ma = ma.to_numpy()
+        ma = ma[index.name]
+
+        if(len(close_array) < p):
+            return 0
+
+        # MAD
+        s = 0
+
+        for i in range(len(close_array),len(close_array)-p,-1):
+            s = s + abs(dataframe['close'][i] - ma)
+        mad = s / p
+
+        # Scalping
+        mcci = diff/mad/0.015
+        
+        return mcci
+
+    
     def trainModelML(self,dataframe):
-            BaseK = 252
             LongWindow = 28
             ShortWindow = 14
+            # DIFF
+            dataframe['mas'] = ta.SMA(dataframe['close'],timeperiod=LongWindow)
+            diffs = dataframe['close'] - dataframe['mas']
+
+            dataframe['maf'] = ta.SMA(dataframe['close'],timeperiod=ShortWindow)
+            difff = dataframe['close'] - dataframe['maf']
 
             # 3paris of predictor indicators , long,short each
             dataframe['rs'] = ta.RSI(dataframe['close'],timeperiod=LongWindow)
-            dataframe['cs'] = ta.CCI(dataframe['high'],dataframe['low'],dataframe['close'],timeperiod=LongWindow)
+            dataframe['cs'] = dataframe.apply((lambda index: self.calculate_CCI(dataframe,index,diffs,dataframe['mas'],LongWindow)),axis=1)
             dataframe['os'] = ta.ROC(dataframe['close'],timeperiod=LongWindow)
             dataframe['vs'] = dataframe.apply((lambda x: self.minimax(dataframe['volume'],x,LongWindow,0,99)),axis=1)
 
             dataframe['rf'] = ta.RSI(dataframe['close'],timeperiod=ShortWindow)
-            dataframe['cf'] = ta.CCI(dataframe['high'],dataframe['low'],dataframe['close'],timeperiod=ShortWindow)
+            dataframe['cf'] = dataframe.apply((lambda index: self.calculate_CCI(dataframe,index,difff,dataframe['maf'],ShortWindow)),axis=1)
             dataframe['of'] = ta.ROC(dataframe['close'],timeperiod=ShortWindow)
             dataframe['vf'] = dataframe.apply((lambda x: self.minimax(dataframe['volume'],x,ShortWindow,0,99)),axis=1)
 
@@ -426,37 +299,31 @@ class TestStrategy(IStrategy):
             return dataframe
     
     def getPredictedValueFromKNN(self,x,dataframe):
-        maxdist = -999.0
+        maxdist = -990.0
+        max_predict_candle = 100
         k = math.floor(math.sqrt(252))
         predictions = []
 
         feature_array_1 = dataframe['f1'].to_numpy()
-        feature_array_1 = feature_array_1[:x.name]
 
         feature_array_2 = dataframe['f2'].to_numpy()
-        feature_array_2 = feature_array_2[:x.name]
 
         direction_array = dataframe['class_label'].to_numpy()
-        direction_array = direction_array[:x.name]
-
-        if(len(feature_array_1) < 1):
-            return 0
-        elif(len(feature_array_2) < 1):
-            return 0
         
-        current_feature1 =  feature_array_1[len(feature_array_1) - 1] 
-        current_feature2 =  feature_array_2[len(feature_array_2) - 1] 
-        size = len(feature_array_1)
-        for i in range(0,size-1):
+        current_feature1 =  feature_array_1[x.name] 
+        current_feature2 =  feature_array_2[x.name] 
+        size = x.name - 100
+
+        # 0,size
+        for i in range(x.name,max_predict_candle,-1):
             d = math.sqrt(math.pow(current_feature1 - feature_array_1[i], 2) + math.pow(current_feature2 - feature_array_2[i], 2))
             if(d > maxdist):
                 maxdist = d
                 if(len(predictions) >= k):
                     predictions.pop()
                 predictions.append(direction_array[i])
-
         prediction = np.sum(predictions)
-        return prediction
+        return -prediction
     
 
     def adaptiveTrendFinder_2(self,dataframe:DataFrame):
